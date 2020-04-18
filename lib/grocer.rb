12feeb54444ad76350af1ef1153491214afaa5e2
Jessica K <jessikim807@gmail.com>
2020-04-18 15:33:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |grocery_item, data|
-    if grocery_item == :item
+    if name == collection[:item]
+      return grocery_item
     end
   end
 end

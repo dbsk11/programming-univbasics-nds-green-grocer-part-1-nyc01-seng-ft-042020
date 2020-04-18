1791f4bd37850cb7178827a52473ae5f71c24f26
Jessica K <jessikim807@gmail.com>
2020-04-18 15:51:18 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   index = 0
   while index < collection.length do
     if collection = name
-      return collection[]
+      return collection[0]
     end
   end
 end
